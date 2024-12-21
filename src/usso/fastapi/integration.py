3fbf4e844be5bb09569016a24354d712f4abb34b
Mahdi Kiani <mahdikiany@gmail.com>
2024-12-21 14:53:24 +0000

    EXCEPTION_HANDLERS

--- src/usso/fastapi/integration.py
+++ src/usso/fastapi/integration.py
@@ -81,3 +81,8 @@ async def usso_exception_handler(request: Request, exc: USSOException):
         status_code=exc.status_code,
         content={"message": exc.message, "error": exc.error},
     )
+
+
+EXCEPTION_HANDLERS = {
+    USSOException: usso_exception_handler,
+}
